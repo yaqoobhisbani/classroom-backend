3bfa0a3d0597c8f0036e1f415b677ccd641bcc42
Muhammad Yaqoob <yaqoobm91@gmail.com>
2019-11-04 21:52:32 +0500

    Added Messages Mongoose Model in Backend!

---
+++ models/Message.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const messageSchema = mongoose.Schema({
+  text: {
+    type: String,
+    required: true
+  },
+  user: {
+    name: {
+      type: String
+    },
+    id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User"
+    },
+    type: {
+      Object
+    },
+    required: true
+  },
+  classroom: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Classroom",
+    required: true
+  },
+  time: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model("Message", messageSchema);
